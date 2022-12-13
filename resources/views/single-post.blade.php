23d9ded40672ec7edabbf8d83bbb73642da7fb05
ZeeoTwo <g.koral@op.pl>
2022-12-13 17:21:38 +0100

    sdahgjasghjdfasghfu

--- resources/views/single-post.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<p class="text-red-700 text-center"> Tytul: {{ $post['title'] }}</p>
-<p class="text-yellow-600 text-center">{{ $post['content'] }}</p>
-<p class="text-emerald-600 text-left">Autor: {{ $post['author'] }}</p>
-@endsection
